f227bc9d042b64003de15e9d8030c1c842e55a99
yanchanglu <513894105@qq.com>
2017-10-09 10:26:10 +0800

    合并冲突

--- src/assets/js/const-value.js
+++ src/assets/js/const-value.js
@@ -1,17 +1,9 @@
 // 右键
 export const RIGHT_CODE = 2
 // TOKEN
-const JWT_TOKEN = 'JWT_TOKEN'
+export const JWT_TOKEN = 'JWT_TOKEN'
 
 // url
 // 目录树
-<<<<<<< HEAD
-export const FOLDER_TREE = 'api/cloud/authFolder'
-// 登录接口
-export const LOGIN_URL = 'api/users/signIn'
-// 注册新用户接口
-export const SIGUP_URL = 'api/users/signUp'
-=======
 export const FOLDER_TREE = '/api/cloud/authFolder'
 export const LOGIN_URL = '/api/users/signIn'
->>>>>>> 4e33679a387495b01c87f6ff845fdfbeca450512
